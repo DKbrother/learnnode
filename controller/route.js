01b0b54a8c1544986bfb5fa496e1fafd1dd24e10
dukai <dukai@letsiot.com>
2019-03-30 15:47:36 +0800

    测试

--- controller/route.js
+++ controller/route.js
@@ -1,28 +1,30 @@
 var optfile = require('./optfile');
 var url = require('url');
-var querystring = require('querystring');		//post请求需导入
+var querystring = require('querystring'); //post请求需导入
 
-var getHtmlCallBack = function (req,res,post) {
-	res.writeHead(200,{'Content-Type':'text/html;charset = utf-8'});
+var getHtmlCallBack = function (req, res, post) {
+	res.writeHead(200, {
+		'Content-Type': 'text/html;charset = utf-8'
+	});
 
 	var callback = function (data) {
 		data = data.toString();
 		//post对象是否为空
-		if(JSON.stringify(post) != '{}'){
-			data = data.replace(/{{email}}/,post["email"])
-					.replace(/{{password}}/,post["password"]);
-		}else{
-			data = data.replace(/{{email}}/,"")
-					.replace(/{{password}}/,"");
+		if (JSON.stringify(post) != '{}') {
+			data = data.replace(/{{email}}/, post["email"])
+				.replace(/{{password}}/, post["password"]);
+		} else {
+			data = data.replace(/{{email}}/, "")
+				.replace(/{{password}}/, "");
 		}
 		res.write(data);
 		res.end();
-	} 
+	}
 	return callback;
 }
 
 module.exports = {
-	login:function (req,res,pathname) {
+	login: function (req, res, pathname) {
 		//get请求接受参数
 		/*  var rest = url.parse(req.url,true).query;
 		if(rest["email"] != undefined){
@@ -31,16 +33,14 @@ module.exports = {
 		} */
 
 		var post = "";
-		req.on('data',function (chunk) {
+		req.on('data', function (chunk) {
 			post += chunk;
 		});
-		req.on('end',function () {
+		req.on('end', function () {
 			post = querystring.parse(post);
-			var callback = getHtmlCallBack(req,res,post);
+			var callback = getHtmlCallBack(req, res, post);
 			var path = `../view/${pathname}.html`;
-			optfile.readfile(path,callback);
+			optfile.readfile(path, callback);
 		});
-
-		
 	}
 }
--- controller/server.js
+++ controller/server.js
@@ -2,19 +2,25 @@ var http = require('http');
 var router = require('./route');
 var url = require('url');
 
-http.createServer(function (req,res) {
+http.createServer(function (req, res) {
 	var pathname = url.parse(req.url).pathname;
-	pathname = pathname.replace(/\//,'');
-	if(req.url != '/favicon.ico'){
-		try{
-			router[pathname](req,res,pathname);
-		}catch (err) {
+	pathname = pathname.replace(/\//, '');
+	if (pathname) {
+		try {
+			router[pathname](req, res, pathname);
+		} catch (err) {
 			console.log(`抛出异常:${err}`);
-			res.writeHead(200,{'Content-Type':'text/html;charset = utf-8'});
+			res.writeHead(200, {
+				'Content-Type': 'text/html;charset = utf-8'
+			});
 			res.write("抛出异常:" + err.toString().split(":")[1]);
-			res.end();
 		}
+	} else {
+		res.writeHead(200, {
+			'Content-Type': 'text/html;charset = utf-8'
+		});
+		res.write('Welcome Node.js!');
 	}
-
+	res.end();
 }).listen(8000);
 console.log('Server running at 127.0.0.1:8000 ...');
